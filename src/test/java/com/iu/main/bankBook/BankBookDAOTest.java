9558dff7b6bfb143ef953849ece0684a2ca94b81
koehdcks <chan3_3@naver.com>
2023-07-26 11:05:48 +0900

    commit

--- src/test/java/com/iu/main/bankBook/BankBookDAOTest.java
+++ src/test/java/com/iu/main/bankBook/BankBookDAOTest.java
@@ -16,9 +16,17 @@ public class BankBookDAOTest extends Mytest{
 	private BankBookDAO bankBookDAO;
 	
 	@Test
-	public void getListTest() throws Exception{
-		List<BankBookDTO> ar = bankBookDAO.getList();
-		assertNotEquals(0, ar.size());
+	public void addTest() throws Exception {
+		BankBookDTO bankBookDTO = new BankBookDTO();
+		
+		for(int i=0;i<30;i++) {
+			bankBookDTO.setBookName("찬찬찬"+i);
+			bankBookDTO.setBookContents("찬찬찬 내용"+i);
+			bankBookDTO.setBookRate(2.3);
+			bankBookDTO.setBookSale(1);
+			bankBookDAO.setAdd(bankBookDTO);
+		}
+		System.out.println("Finish");
 		
 	}
 	
